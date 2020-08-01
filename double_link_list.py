848702efcea1918844087cbe82c1ea2ed2caa16e
JAMSHID12 <61357863+JAMSHID12@users.noreply.github.com>
2020-08-01 16:38:34 +0530

    Update double_link_list.py

--- double_link_list.py
+++ double_link_list.py
@@ -1,11 +1,69 @@
-# find duplicate number in list
-def duplicate(ls):
-    size = len(ls)
-    for i in range(0, size):
-        for j in range(i+1, size):
-            if ls[i] == ls[j]:
-                print(ls[i], end=" ")
+class Node:
+    def __init__(self, prev=None,data=None, next=None):
+        self.data = data
+        self.prev = prev
+        self.next = next
+class Double_linked_list:
+    def __init__(self):
+        self.head = None
 
+    def inser_beggining(self, data):
+        node = Node(None, data, self.head)
+        self.head.prev = node
+        self.head = node
 
-ls = [1, 8, 3, 4, 5, 5, 7, 8, 4, 7, 9, 9]
-duplicate(ls)
+    def print_list_forword(self):
+        if self.head is None:
+            print('linklist is empty')
+            return
+        itr = self.head
+        llist = ''
+        while itr:
+            llist += str(itr.data) + '-->'
+            itr = itr.next
+        print(llist)
+
+    def print_backword(self):
+        if self.head is None:
+            print('list is empty')
+            return
+        last_node = self.last_node()
+        itr = last_node
+        llstr = ' '
+        while itr:
+            llstr += '<--' + str(itr.data)
+            itr = itr.prev
+        print(llstr)
+
+    def last_node(self):
+        itr = self.head
+        while itr:
+            itr = itr.next
+        return itr
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            itr = itr.next
+            count += 1
+        return count
+
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(None, data, None)
+            return
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+
+        itr.next = Node(itr, data, None)
+
+    def insert_values(self, data_list):
+        self.head = None
+        for item in data_list:
+            self.insert_at_end(item)
+
+if __name__ == '__main__':
+    dl = Double_linked_list()
+    dl.insert_values(['ram', 'dul'])
+    dl.print_list_forword()
