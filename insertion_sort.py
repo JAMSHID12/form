c87344587a9092f2e9daaea20acd8d79483bd4f7
jamshid ct <jamshidct99@gamil.com>
2020-08-03 20:56:31 +0530

    insertion sort ascending order

---
+++ insertion_sort.py
@@ -0,0 +1,14 @@
+# insertion sort in ascending order
+def insertion_sort(list1):
+    for i in range(len(list1)):
+        pos = i
+        while pos > 0 and list1[pos] < list1[pos-1]:
+            list1[pos], list1[pos-1] = list1[pos-1], list1[pos]
+            pos = pos - 1
+
+    for i in range(len(list1)):
+        print(list1[i], end=' ')
+
+
+A = [4, 3, 7, 5, 6, 9, 8, 2]
+insertion_sort(A)
