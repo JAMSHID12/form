e6e9e1e5fac99d165cf2067d969f48240153fae5
jamshid ct <jamshidct99@gamil.com>
2020-08-03 20:01:24 +0530

    add sort using while condition

--- selectionSort.py
+++ selectionSort.py
@@ -26,4 +26,25 @@ def sorting(list1):
 
 A = [3, 5, 2, 7, 4, 9, 8]
 sorting(A)
+print()
+# selection sort using while condition
+def selection_sort(list1):
+    i = 0
+    while i < len(list1):
+        j = i
+        while j < len(list1):
+            if list1[i] > list1[j]:
+                list1[i], list1[j] = list1[j], list1[i]
+            j += 1
+        i += 1
+
+    i = 0
+    while i < len(list1):
+        print(list1[i], end=" ")
+        i += 1
+
+
+A = [4, 5, 3, 8, 2, 1, 9, 7]
+selection_sort(A)
+
 
